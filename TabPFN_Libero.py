35d2fa58fd4c94281a7630b75a4c71c30878ec83
dskin344 <dskin344@gmail.com>
2026-02-04 14:03:28 -0600

    Working Slow Sim Speed

--- TabPFN_Libero.py
+++ TabPFN_Libero.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+import os
 from pathlib import Path
 
 import h5py
@@ -9,7 +10,7 @@ import jax
 import libero
 from libero.libero import benchmark
 from libero.libero.envs import OffScreenRenderEnv
-from libero.libero.utils.download_utils import check_libero_dataset
+import libero.libero.utils.download_utils
 import numpy as np
 from rich import print
 from sklearn.metrics import mean_squared_error, r2_score
@@ -40,102 +41,89 @@ def extract(task_suite, task_id):
     full_path: Path = suites / suite
 
     tree = h5_to_tree(full_path)
-    # optional: post-process (example: cast all float64 -> float32)
-    # tree = jtu.tree_map(lambda x: x.astype(np.float32) if x.dtype == np.float64 else x, tree)
     demos = tree["data"]
-    d0 = tree["data"]["demo_0"]
-    # print(spec(demos))
 
     sa_by_demo = {k: (d["states"], d["actions"]) for k, d in demos.items()}
-    keys = sorted(sa_by_demo.keys())
+    keys = sa_by_demo.keys()
 
     states = np.concatenate([sa_by_demo[k][0] for k in keys], axis=0)
     actions = np.concatenate([sa_by_demo[k][1] for k in keys], axis=0)
 
-    # states, actions = d0['states'], d0['actions']
     return states, actions
 
 
+def check_download(task_suite_name):
+    suite_dir = suites.joinpath(task_suite_name)
+
+    if os.path.exists(suite_dir):
+        print("Datasets found:")
+        t_names = [f.stem for f in suite_dir.glob("*.hdf5")]
+        for index, name in enumerate(t_names):
+            print(index, ": ", name)
+    else:
+        print("Task suite datasets not found. Downloading now")
+        libero_dataset_download(datasets=task_suite_name, use_huggingface=True)
+
+
 class MyMultiTPFN:
-    def __init__(self, dim: int, **kwargs):
+    def __init__(self, dim: int):
         self.dim = dim
-        self.models = [TabPFNRegressor(**kwargs) for _ in range(dim)]
+        self.models = [TabPFNRegressor() for _ in range(dim)]
 
     def fit(self, x: np.ndarray, y: np.ndarray):
+        print("Fitting...")
         for i in range(self.dim):
             self.models[i].fit(x, y[:, i])  # Train on dimension i
+        print("Done fitting")
 
     def predict(self, x: np.ndarray) -> np.ndarray:
+        print("Predicting...")
         predictions = []
         for i in range(self.dim):
             pred = self.models[i].predict(x)
             predictions.append(pred)
+        print("Done Predicting")
         return np.column_stack(predictions)
 
 
 @dataclass
 class Config:
-    task: str | None = None
+    suite: str = "libero_spatial"
+    task_id: int = 0
+    training_ratio: float = 0.05
 
 
 def main(cfg: Config):
-    task_suite_name = "libero_spatial"
-    task_id = 0
+    task_suite_name = cfg.suite
+    task_id = cfg.task_id
 
     task_suite = benchmark.get_benchmark(task_suite_name)()
     num_tasks = task_suite.get_num_tasks()
     task_names = task_suite.get_task_names()
 
-    if not check_libero_dataset():
-        print("Datasets not found")
-        # libero_dataset_download(datasets="all", use_huggingface=True)
-    else:
-        print("Datasets found, no need to download")
+    check_download(task_suite_name)
 
     features, actions = extract(task_suite, task_id)
     print(features.shape)
     print(actions.shape)
 
-    # Fit the model here
     indices = np.arange(features.shape[0])
     np.random.shuffle(indices)
     features = features[indices]
     actions = actions[indices]
 
-    n_fit = int(features.shape[0] * 0.80)
-    n_test = features.shape[0] - n_fit
+    n_fit = int(features.shape[0] * cfg.training_ratio)
     x_fit, x_test = features[:n_fit], features[n_fit:]
     y_fit, y_test = actions[:n_fit], actions[n_fit:]
 
-    """regressor = TabPFNRegressor()
-    regressor.fit(x_train, y_train)
-    prediction_policy = regressor.predict(x_test)"""
-
     policy = MyMultiTPFN(dim=7)
-    # fit the policy
     policy.fit(x_fit, y_fit)
-    # predict the policy
     yh = policy.predict(x_test)
 
-    # import mse metric from tabpfn.metrics
-    # Evaluate the model
     mse = mean_squared_error(y_test, yh)
     r2 = r2_score(y_test, yh)
-
     print("Mean Squared Error (MSE):", mse)
     print("R² Score:", r2)
-
-    """
-    prediction_policy = []
-    for i in range(7):
-        regressor = TabPFNRegressor()
-        regressor.fit(x_fit, y_fit[:, i])  # Train on dimension i
-        pred = regressor.predict(x_test)
-        prediction_policy.append(pred)
-    """
-
-    # Combine into (69, 7) shape
-    # action_policy = np.column_stack(prediction_policy)
 
     task = task_suite.get_task(task_id)
     bddl_file_path = task_suite.get_task_bddl_file_path(task_id)
@@ -151,24 +139,28 @@ def main(cfg: Config):
 
     # Create environment
     env = OffScreenRenderEnv(**env_args)
-    thing = env.reset()
-    print(spec(thing))
+    env.reset()
 
     frames = []
-    done, step, max_steps = False, 0, 500
+    done, step, max_steps = False, 0, 300
     while not done and step < max_steps:
-        # action = action_policy[step]
-        print(spec(obs))
-        action = policy.predict(obs.reshape(1, -1))
-        obs, reward, done, _info = env.step(action)
+        env_state = env.get_sim_state()
+
+        print(spec(env_state))
+        print("Env State Shape: ", env_state.shape)
+        action = policy.predict(env_state.reshape(1, -1))
+        action = np.concatenate(action, axis=0)
+        print(action.shape)
+
+        obs, _reward, done, _info = env.step(action)
 
         frames.append(obs["galleryview_image"][::-1])
 
-        print(f"step={step}, reward={reward}")
+        print(f"step={step}")
 
         if done:
             print("Resetting the env")
-            obs = env.reset()
+            env.reset()
 
         step += 1
 
