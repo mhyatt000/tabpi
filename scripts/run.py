e88565a08b3ab0fb7283154ee552abe2a8ef4198
matt <mhyatt@luc.edu>
2026-03-02 21:52:52 +0000

    remove dead code

--- scripts/run.py
+++ scripts/run.py
@@ -4,13 +4,13 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any
 
+import libero
 from rich import print
 from tabpfn_extensions.multioutput import TabPFNMultiOutputRegressor
 import torch
 import tyro
 import wandb
 
-import libero
 from tabpi.envs.libero import EnvFactory, LiberoFactory
 from tabpi.models.tabpfn_policy import ModelPolicy
 from tabpi.utils.data import check_download, extract, shuffle
